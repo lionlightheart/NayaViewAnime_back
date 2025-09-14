1a15a093dfbee26bc79b4dbddb99889cd66c08b2
lion <lionlightheart@gmail.com>
2025-09-14 04:07:28 +0200

    Update ALLOWED_HOSTS in settings.py and remove unused test file

--- NayaViewAnime_Back/settings.py
+++ NayaViewAnime_Back/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+2y32d^n@av2jehgdpx&5n#lblpb2ixm3kno1s3opzev7m*&5g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["local.nayaview.com"]
 
 
 # Application definition
@@ -82,7 +82,6 @@ DATABASES = {
         'PORT': '5432',                              # Puerto de PostgreSQL
     }
 }
-
 
 
 # Password validation
